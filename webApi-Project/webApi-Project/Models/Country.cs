f0655865a981100cdf613592c6d14af4034fb236
Norayr Sukiasyan <norayr_sukiasyan@edu.aua.am>
2024-05-24 21:29:41 +0400

    Added models

---
+++ webApi-Project/webApi-Project/Models/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace webApi_Project.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ webApi-Project/webApi-Project/Models/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace webApi_Project.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public String Gym { get; set; }
+    }
+
+}
