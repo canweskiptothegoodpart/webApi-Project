95a2481aa3d62e918b8b10765c31111cd04b2174
Norayr Sukiasyan <norayr_sukiasyan@edu.aua.am>
2024-05-24 21:48:15 +0400

    Added one-to-many relationships

--- webApi-Project/webApi-Project/Models/Owner.cs
+++ webApi-Project/webApi-Project/Models/Owner.cs
@@ -4,8 +4,10 @@
     {
         public int Id { get; set; }
         public String Name { get; set; }
-
         public String Gym { get; set; }
+
+        public Country Country { get; set; }
+
     }
 
 }
